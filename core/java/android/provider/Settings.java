42ddc9c1aa28d147c19c99e9ca1b37666d1cb3ef
rascarlo <rascarlo@gmail.com>
2015-12-01 19:18:53 +0100

    Volume Rocker Wake [1/2]
    Change-Id: Ia32e77b4737efd5816da1b4cfb74138fc357a46b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3536,6 +3536,13 @@ public final class Settings {
          */
 
         /**
+         * Volume rocker wake
+         * @hide
+         */
+        public static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
